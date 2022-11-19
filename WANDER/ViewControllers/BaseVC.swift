24394bc577c1ab80939b86fbdb3742b7a837abdb
Cory Tepper <corytepper@gmail.com>
2022-11-19 13:43:10 -0500

    Initial Commit

---
+++ WANDER/ViewControllers/BaseVC.swift
@@ -0,0 +1,16 @@
+//
+//  BaseVC.swift
+//  WANDER
+//
+//  Created by Cory Tepper on 11/19/22.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange.withAlphaComponent(0.3)
+    }
+}
