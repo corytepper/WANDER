a0c0adebaf560bdbf0187e2d218f49022edd0180
Cory Tepper <corytepper@gmail.com>
2022-11-22 08:37:50 -0500

    setup Realm models

---
+++ WANDER/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WANDER
+//
+//  Created by Cory Tepper on 11/22/22.
+//
+
+import Foundation
+
+final class Constants {
+    static let realmQueue = DispatchQueue(label: "RealmQueue")
+}
